1c20d7da99843238ba01fb4e91abe3d52d3d192b
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2026-04-12 11:42:36 +0200

    al

--- .github/scripts/install_llvm_ubuntu.sh
+++ .github/scripts/install_llvm_ubuntu.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-v=22
+v=${1:-22}
 bins=(clang llvm-config lld ld.lld FileCheck)
 llvm_sh=$(mktemp)
 
 wget https://apt.llvm.org/llvm.sh -O "$llvm_sh"
 chmod +x "$llvm_sh"
-"$llvm_sh" "$v"
+"$llvm_sh" "$v" all
 for bin in "${bins[@]}"; do
     if ! command -v "$bin-$v" &>/dev/null; then
         echo "Warning: $bin-$v not found" 1>&2
